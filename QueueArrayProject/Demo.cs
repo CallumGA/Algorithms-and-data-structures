4eea46ceb85128a69e9be2c3f06f1b9158029b84
callu <callu@LAPTOP-OIMGLDQI>
2020-07-29 19:27:46 -0400

    Queue array project

---
+++ QueueArrayProject/Demo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueArrayProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+
+            QueueA queue = new QueueA();
+
+            while (true)
+            {
+                Console.WriteLine("1. Insert element to queue");
+                Console.WriteLine("2. Remove an element from the queue");
+                Console.WriteLine("3. Display the front element");
+                Console.WriteLine("4. Display all queue elements");
+                Console.WriteLine("5. Display the size of the queue");
+                Console.WriteLine("6. Quit");
+
+                Console.WriteLine("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 6) break;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter element to insert: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        queue.Insert(x);
+                        break;
+                    case 2:
+                        x = queue.Delete();
+                        Console.WriteLine("Deleted element is:  " + x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Element at front is: " + queue.Peek());
+                        break;
+                    case 4:
+                        queue.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Size of queue is: " + queue.Size());
+                        break;
+                    default:
+                        Console.WriteLine("Choice not in list");
+                        break;
+                }
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
---
+++ QueueArrayProject/QueueA.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueArrayProject
+{
+    class QueueA
+    {
+        private int[] queueArray;
+        private int front;
+        private int rear;
+
+        public QueueA()
+        {
+            queueArray = new int[10];
+            front = -1;
+            rear = -1;
+        }
+
+        public QueueA(int maxSize)  
+        {
+            queueArray = new int[maxSize];
+            front = -1;
+            rear = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return (front == -1 || front==rear + 1);
+        }
+
+        public bool IsFull()
+        {
+            return (rear == queueArray.Length - 1);
+        }
+
+        public int Size()
+        {
+            if (IsEmpty())
+                return 0;
+            else
+                return rear - front + 1;
+        }
+
+        public void Insert(int x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue overflow!");
+                return;
+            }
+                
+            if (front == -1)
+                front = 0;
+            rear = rear + 1;
+            queueArray[rear] = x;   
+        }
+
+        public int Delete()
+        {
+            int x;  
+            if (IsEmpty())
+                Console.WriteLine(" Queue underflow!");
+            x = queueArray[front];
+            front = front + 1;
+
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                Console.WriteLine(" Queue underflow!");
+            return queueArray[front];
+        }
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty!");
+                return;
+            }
+
+            Console.WriteLine("Queue is : \n\n");
+            for (int i = front; i <= rear; i++)
+            {
+                Console.WriteLine(queueArray[i] + " ");
+            }
+
+            Console.WriteLine();
+
+        }
+
+    }
+}

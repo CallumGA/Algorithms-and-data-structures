f4086ea9b9e239da1051bef253ad677e06744cdc
Cal <Cal@DESKTOP-66H5HFU>
2020-08-06 17:37:26 -0400

    Recursion 1 to n example

---
+++ Recursion1toN/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Recursion1toN
+{
+    class Program
+    {
+        //Left off here - review this code
+
+        public static void Print1(int n)
+        {
+            if (n == 0)
+                return;
+            Console.Write(n + " ");
+            Print1(n-1);
+        }
+
+        public static void Print2(int n)
+        {
+            if (n == 0)
+                return;    
+            Print2(n - 1);
+            Console.Write(n + " ");
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter value of n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Print1(n);
+            Console.WriteLine();
+
+            Print2(n);
+            Console.WriteLine();
+        }
+    }
+}
--- RecursionFactorial/Program.cs
+++ RecursionFactorial/Program.cs
@@ -9,21 +9,23 @@ namespace RecursionFactorial
 
         static void Main(string[] args)
         {
-            int n;
+            int n;//n for the inputed value you want the factorial of
 
             Console.WriteLine("Enter a number greater than zero : ");
-            n = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());//Set the value
 
-            Console.WriteLine("Factorial of " + n + " is " + Factorial(n));
+            Console.WriteLine("Factorial of " + n + " is " + Factorial(n));//Call method pass in value
 
 
         }
 
         private static long Factorial(int n)
         {
-            if (n == 0)
+            if (n == 0)//Check to make sure input value is not 0
                 return 1;
-            return n * Factorial(n - 1);
+            return n * Factorial(n - 1);//Multiply value of n by n-1 passed back into the factorial method, thus multiplying
+                                        //by the next number down
+                                        //6 x 5 then 5 x 4 then 4 x 3...etc
         }
     }
 }

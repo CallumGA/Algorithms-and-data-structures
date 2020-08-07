e6e6450cd554cb1e9247eeb708ac9739e3795306
Cal <Cal@DESKTOP-66H5HFU>
2020-08-07 17:52:08 -0400

    Get sum of digits of n number, recursion

--- Recursion1toN/Program.cs
+++ Recursion1toN/Program.cs
@@ -4,22 +4,21 @@ namespace Recursion1toN
 {
     class Program
     {
-        //Left off here - review this code
-
-        public static void Print1(int n)
+        public static void Print1(int n)//Number user inputted
         {
-            if (n == 0)
+            if (n == 0)//Make sure not 0
                 return;
-            Console.Write(n + " ");
-            Print1(n-1);
+            Console.Write(n + " ");//Print the number
+            Print1(n-1);//Subtract 1 from the previous number and run it through Print1 again(thus counting down)
         }
 
-        public static void Print2(int n)
+        public static void Print2(int n)//
         {
-            if (n == 0)
+            if (n == 0)//Make sure not null
                 return;    
-            Print2(n - 1);
-            Console.Write(n + " ");
+            Print2(n - 1);//This call runs through first(runs Print2 as many times as the size of the number) with no printing, move on when reached the base case
+            Console.Write(n + " ");//Go back up the stack and print the numbers in reverse(when we get to this method we have gotten to base case)
+                                   //This is why when we start printing, it starts at 1 and counts up
         }
 
         static void Main(string[] args)
---
+++ SumOfDigitsRecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Transactions;
+
+namespace SumOfDigitsRecursion
+{
+    /* Get the sum of a numbers digits via recursion */
+
+    //Left off here - review this code
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(SumDigits(n));
+
+        }
+
+        private static int SumDigits(int n)
+        {
+            if (n / 10 == 0)
+                return n;
+            return SumDigits(n / 10) + n % 10;
+        }
+    }
+}

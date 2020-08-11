5c9c5e303c17400be0efaa8b5bde65d7feda1bf6
Cal <Cal@DESKTOP-66H5HFU>
2020-08-11 14:52:57 -0400

    Leetcode bubble sort for two sum

--- LeetcodePractice/Program.cs
+++ LeetcodePractice/Program.cs
@@ -1,42 +1,59 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace LeetcodePractice
 {
 
-    class Result
+    //Leetcode bubble sort for two sum question
+
+    class Solution
     {
-        public static List<int> gradingStudents(List<int> grades)
+
+        public static int[] TwoSum(int[] nums, int target)
         {
-            for (int i = 0; i < grades.Count; i++)
-                if ((grades[i] + 4) / 5 * 5 - grades[i] < 3 && grades[i] >= 38)
-                    grades[i] = (grades[i] + 4) / 5 * 5;
-            return grades;
+            List<int> result = new List<int>();
+
+            for (int write = 0; write < nums.Length - 1; write++)
+            {
+                for (int sort = 1; sort < nums.Length; sort++)
+                {
+                    if (nums[sort] + nums[write] == target && sort != write && result.Count == 0)
+                    {
+
+                        Console.WriteLine("Found");
+                        Console.WriteLine(write + ", " + sort);
+                        result.Add(write);
+                        result.Add(sort);
+                        break;
+
+                    }
+
+                }
+            }
+
+            return result.ToArray();
+
         }
-    }
 
-
-    class Program
-    {
 
         public static void Main(string[] args)
         {
-
-
-            List<int> grades = new List<int>();
-            grades.Add(73);
-            grades.Add(67);
-            grades.Add(38);
-            grades.Add(33);
-
-
-            List<int> result = Result.gradingStudents(grades);
-            foreach (int i in result)
-            {
-                Console.WriteLine(i);
-            }
-
+            int[] nums = new int[] { 2, 5, 5, 11 };
+            TwoSum(nums, 10);
         }
+
+
     }
 }
 
